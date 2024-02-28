8be4c3f5a572095f5cda1b88a4a1b449d805e121
brassc <cmb247@cam.ac.uk>
2024-02-27 18:40:43 -0800

    new function to translate and rotate coordinates to lie on x axis with start point at (0, 0)

--- points_plotting/translate_rotate.py
+++ points_plotting/translate_rotate.py
@@ -1,6 +1,7 @@
 import numpy as np
+from numpy.polynomial.polynomial import Polynomial
 
-def move(h, v):
+def move(h, v, poly=0):
     rotation_angle = np.arctan((v[-1]-v[0])/(h[0]-h[-1]))
 
     # rotation matrix
@@ -11,7 +12,6 @@ def move(h, v):
     group_coords=np.vstack((h, v))
     rot_coords=np.dot(rotation_matrix, group_coords)
 
-
     left_magnitude=rot_coords[0,-1]
     down_magnitude=rot_coords[1, 0]  
     tr_v_coords=rot_coords[1]-down_magnitude
@@ -19,4 +19,16 @@ def move(h, v):
 
     print(f"Rotation angle: {rotation_angle}, Translation by ({left_magnitude}, {down_magnitude})")
 
-    return tr_h_coords, tr_v_coords
+    if poly != 0:
+        coefficients_rotated = np.dot(rotation_matrix, poly.coef)
+        poly_rotated = Polynomial(coefficients_rotated)
+        translation_vector = np.array([left_magnitude, down_magnitude])
+        poly_rt_coef = poly_rotated + translation_vector
+        poly_rt=Polynomial(poly_rt_coef)
+        print(f"Polynomial:\n {poly_rt}")
+        return tr_h_coords, tr_v_coords, poly_rt
+    else:
+        return tr_h_coords, tr_v_coords, None
+
+
+    
