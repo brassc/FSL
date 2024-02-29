c9f9995f457a72ef7821cf82016590c17e2e0c70
brassc <cmb247@cam.ac.uk>
2024-02-29 18:02:16 +0000

    update to nclude patient_timepoint in file name

--- BET_Scripts/slice_bet_script.sh
+++ BET_Scripts/slice_bet_script.sh
@@ -10,6 +10,8 @@ input_image="/home/cmb247/Desktop/Project_3/BET_Extractions/19978/T1w_time1_regi
 directory='/home/cmb247/Desktop/Project_3/BET_Extractions/19978/T1w_time1_registered_scans/'
 output_image="${directory}betslice_T1w_time1.T1w_verio_P00030_19978_acute_20111102_U-ID22791_registered.nii.gz"
 input_cut="${directory}cut_T1w_time1.T1w_verio_P00030_19978_acute_20111102_U-ID22791_registered.nii.gz"
+patient_id='19978'
+patient_timepoint='acute'
 
 # BET before slicing
 # cut neck off
@@ -17,19 +19,19 @@ cut_above='56'
 bet_params='-f 0.5 -R'
 bet_p=$(echo "$bet_params" | tr -d ' ')
 fslroi $input_image $input_cut 0 -1 0 -1 $cut_above -1
-bet ${input_cut} ${directory}bet${bet_p} $bet_params
+bet ${input_cut} ${directory}${patient_timepoint}_bet${bet_p} $bet_params
 
 
 # add neck blank box back in
 # Determine dimensions of the original and cut images to calculate the removed slices
 original_dim_z=$(fslval $input_image dim3)
-bet_dim_z=$(fslval ${directory}bet${bet_p} dim3)
+bet_dim_z=$(fslval ${directory}${patient_timepoint}_bet${bet_p} dim3)
 # Assume ${directory}/bet${bet_p} is your BET processed image
-x_dim=$(fslval ${directory}/bet${bet_p} dim1)
-y_dim=$(fslval ${directory}/bet${bet_p} dim2)
-x_pixdim=$(fslval ${directory}/bet${bet_p} pixdim1)
-y_pixdim=$(fslval ${directory}/bet${bet_p} pixdim2)
-z_pixdim=$(fslval ${directory}/bet${bet_p} pixdim3)
+x_dim=$(fslval ${directory}${patient_timepoint}_bet${bet_p} dim1)
+y_dim=$(fslval ${directory}${patient_timepoint}_bet${bet_p} dim2)
+x_pixdim=$(fslval ${directory}${patient_timepoint}_bet${bet_p} pixdim1)
+y_pixdim=$(fslval ${directory}${patient_timepoint}_bet${bet_p} pixdim2)
+z_pixdim=$(fslval ${directory}${patient_timepoint}_bet${bet_p} pixdim3)
 
 
 # Calculate the number of slices removed during the cut
@@ -42,11 +44,11 @@ fslcreatehd $x_dim $y_dim $removed_slices 1 $x_pixdim $y_pixdim $z_pixdim 1 0 0 
 
 
 # Concatenate the empty image back to the BET output along the Z-axis
-fslmerge -z ${directory}restored_bet${bet_p} ${directory}empty ${directory}bet${bet_p}
+fslmerge -z ${directory}${patient_timepoint}_restored_bet${bet_p} ${directory}empty ${directory}${patient_timepoint}_bet${bet_p}
 
 
 # Create binary mask
-fslmaths ${directory}restored_bet${bet_p} -bin ${directory}restored_bet_mask${bet_p}
+fslmaths ${directory}${patient_timepoint}_restored_bet${bet_p} -bin ${directory}${patient_timepoint}_restored_bet_mask${bet_p}
 
 
 # Extract one slice
