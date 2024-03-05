d91f32ee77d8f40c036252fc348936a878e80b3d
brassc <cmb247@cam.ac.uk>
2024-03-04 19:27:54 -0800

    extract all areas

--- points_plotting/main.py
+++ points_plotting/main.py
@@ -19,17 +19,17 @@ nifti_file_path ='/home/cmb247/Desktop/Project_3/BET_Extractions/19978/T1w_time1
 bet_mask_file_path="/home/cmb247/Desktop/Project_3/BET_Extractions/19978/T1w_time1_registered_scans/acute_restored_bet_mask-f0.5-R.nii.gz"
 slice_selected=np.array([2.641497, -2.877373, -12.73399,1]) # Scanner coordinates
 #voxel loc: 91 119 145
-#extract_data_make_plots(patient_id, patient_timepoint, nifti_file_path, slice_selected, scatter=False, deformed_order=2)
+extract_data_make_plots(patient_id, patient_timepoint, nifti_file_path, slice_selected, scatter=False, deformed_order=2)
 #auto_boundary_detect(patient_id, patient_timepoint, bet_mask_file_path, x_offset=120, array_save_name='deformed_boundary.npz')
 #auto_boundary_detect(patient_id, patient_timepoint, bet_mask_file_path, x_offset=50, array_save_name='baseline_boundary.npz')
-
+## x_offset VARIABLE SETS X DIMENSION FOR ZOOMED IN PLOT WINDOW
 patient_timepoint='fast'
 nifti_file_path ='/home/cmb247/Desktop/Project_3/BET_Extractions/19978/T1w_time1_registered_scans/T1w_time1.T1w_verio_P00030_19978_fast_20111027_U-ID22723_registered.nii.gz'
 #slice_selected=np.array([2.641497, -2.877373, -12.73399,1]) # Scanner coordinates
 #voxel loc: 91 119 145
 slice_selected=int(145)
 
-#extract_data_make_plots(patient_id, patient_timepoint, nifti_file_path, slice_selected, scatter=True, deformed_order=4)
+extract_data_make_plots(patient_id, patient_timepoint, nifti_file_path, slice_selected, scatter=True, deformed_order=4)
 #auto_boundary_detect(patient_id, patient_timepoint, bet_mask_file_path, x_offset=120, array_save_name='deformed_boundary.npz')
 #auto_boundary_detect(patient_id, patient_timepoint, bet_mask_file_path, x_offset=50, array_save_name='baseline_boundary.npz')
 
@@ -39,13 +39,13 @@ patient_timepoint='acute'
 nifti_file_path = '/home/cmb247/Desktop/Project_3/BET_Extractions/19344/T1w_time1_registered_scans/T1w_time1.T1w_verio_P00244_19344_acute_20110323_U-ID21262_registered.nii.gz'
 slice_selected=np.array([29.85494, -76.71205, 22.77727, 1]) # Scanner coordinates
 
-#extract_data_make_plots(patient_id, patient_timepoint, nifti_file_path, slice_selected, scatter=True, deformed_order=3)
+extract_data_make_plots(patient_id, patient_timepoint, nifti_file_path, slice_selected, scatter=True, deformed_order=4)
 
 patient_timepoint='fast'
 nifti_file_path='/home/cmb247/Desktop/Project_3/BET_Extractions/19344/T1w_time1_registered_scans/T1w_time1.T1w_verio_P00244_19344_fast_20110308_U-ID21134_registered.nii.gz'
 slice_selected=int(155)
 
-extract_data_make_plots(patient_id, patient_timepoint, nifti_file_path, slice_selected, scatter=False, deformed_order=2)
+extract_data_make_plots(patient_id, patient_timepoint, nifti_file_path, slice_selected, scatter=True, deformed_order=4)
 
 patient_id='22725'
 patient_timepoint='acute'
