a68e5c77d2e164e2112873ed875666747450436d
brassc <cmb247@cam.ac.uk>
2024-03-04 19:46:49 -0800

    run omain.py and record results

--- points_plotting/omain.py
+++ points_plotting/omain.py
@@ -15,5 +15,22 @@ rt_data_make_plots(patient_id, patient_timepoint)
 patient_timepoint="fast"
 rt_data_make_plots(patient_id, patient_timepoint)
 
+patient_id='19344'
+patient_timepoint='acute'
+rt_data_make_plots(patient_id, patient_timepoint)
+
+patient_timepoint='fast'
+rt_data_make_plots(patient_id, patient_timepoint)
+
+patient_id='22725'
+patient_timepoint='acute'
+rt_data_make_plots(patient_id, patient_timepoint)
+
+patient_id='13990'
+patient_timepoint='acute'
+rt_data_make_plots(patient_id, patient_timepoint)
+
+
+
 
  
