3d9c17b510c581fd204b1ec3f10a0c590ab77cd2
brassc <cmb247@cam.ac.uk>
2024-07-10 15:29:11 +0100

    choose input scan basename based off patient timepoint input, with accommodation for fast vs ultra-fast

--- Image_Processing_Scripts/trial1.sh
+++ Image_Processing_Scripts/trial1.sh
@@ -2,21 +2,48 @@
 #Load modules
 module load fsl
 
-# Define input parameters
+## PATIENT PARAMETERS
 patient_id="19978"
-input_directory="/home/cmb247/Desktop/Project_3/BET_Extractions/19978/OG_Scans/" # make sure to put / at end of directory
 timepoint="fast"
-input_basename="T1w_verio_P00030_19978_fast_20111027_U-ID22723.nii.gz"
-input_basename_without_extension="${input_basename%.*}"
+
+##SET REMAINING VARIABLES
+## Define remaining input parameters
+input_directory="/home/cmb247/Desktop/Project_3/BET_Extractions/$patient_id/OG_Scans/" # make sure to put / at end of directory
+input_basename="initialise"
+#find input scan basename based on timepoint input
+while IFS= read -r file; do
+    # Extract basename of file
+    basename=$(basename "$file")
+    
+    # Check if the file name contains "T1" and "$timepoint"
+    if [[ "$basename" == "T1"* && "$basename" == *"$timepoint"* && "$basename" == *".nii.gz" ]]; then 
+        # Exclude files that start with "ultra-" before search term (i.e. "ultra-fast" is fine)
+        if [[ "$timepoint" == "fast" && "$basename" != *"ultra-fast"* ]]; then
+            input_basename=$basename
+            echo "Input basename set as $input_basename."
+        elif [[ "$timepoint" == "fast" && "$basename" == *"ultra-fast"* ]]; then
+            echo "skipping ultra-fast basename allocation for $timepoint timepoint."
+        else
+            input_basename=$basename
+            echo "Input basename set as $input_basename."
+        fi
+
+    fi
+done < <(find "$input_directory" -type f)
+
+echo "Final input basename is $input_basename"
+input_basename_without_extension="${input_basename%.nii.gz}"
 input_image="${input_directory}${input_basename}"
+echo "$input_image"
 
 # Define output parameters
-output_directory="/home/cmb247/Desktop/Project_3/BET_Extractions/19978/OG_Scans_bias_corr/" # make sure to put / at end of directory
+output_directory="/home/cmb247/Desktop/Project_3/BET_Extractions/$patient_id/OG_Scans_bias_corr/" # make sure to put / at end of directory
 output_basename="${input_basename_without_extension}_bias_corr.nii.gz"
 output_image="${output_directory}${output_basename}"
 
 
 
+## VERIFY INPUT IMAGE
 # Verify input image
 if [ ! -f "$input_image" ]; then
     echo "Error: input_image ${input_image} does not exist."
@@ -24,6 +51,8 @@ if [ ! -f "$input_image" ]; then
 fi
 echo "starting bias correction for input_image: $input_image..."
 
+
+## STARTING BIAS CORRECTION
 # create bias correction output directory OG_Scans_bias_corr
 if [ -d $output_directory ]; then
     echo "Output directory ${output_directory} exists."
@@ -44,9 +73,6 @@ else
     echo "Bias field correction failed."
     exit 1
 fi
-
-
-
 
 
 chmod +x trial1.sh
