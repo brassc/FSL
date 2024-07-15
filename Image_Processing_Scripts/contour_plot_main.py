c039aea275b9a4bce8cc85bb1ac86fdabf175a49
brassc <cmb247@cam.ac.uk>
2024-07-15 18:13:15 +0100

    add search functionality, search for mask by patient ID, timepoint, with 'modified' masks given priority

--- Image_Processing_Scripts/contour_plot_main.py
+++ Image_Processing_Scripts/contour_plot_main.py
@@ -328,15 +328,18 @@ patient_info.drop(columns='excluded', inplace=True)
 
 
 # Iterate over each patient and timepoint
-for patient_id, timepoint in zip(patient_ids, timepoints):
+for patient_id, timepoint in zip(patient_info['patient_id'], patient_info['timepoint']):
     # Define the bet_mask_file_path for each patient and timepoint
     directory = f"/home/cmb247/Desktop/Project_3/BET_Extractions/{patient_id}/T1w_time1_bias_corr_registered_scans/BET_Output"
     # Construct the search pattern
     pattern = f"*{timepoint}*_bet_mask*.nii.gz"
+    pattern_priority = f"*{timepoint}*_bet_mask*modified*.nii.gz"
     # Search for files matching the pattern in the specified directory
-    file = glob.glob(os.path.join(directory, pattern))
     
-    print(file.path())
+    file = glob.glob(os.path.join(directory, pattern_priority))
+    if not file:
+        file = glob.glob(os.path.join(directory, pattern))
+    print(file)
     # Call the auto_boundary_detect function for each patient and timepoint
     #auto_boundary_detect(patient_id, timepoint, bet_mask_file_path, x_offset, array_save_name)
 
