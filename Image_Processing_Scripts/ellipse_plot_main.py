8e6092abb2589bec8cfed7743324957a7a6a962d
brassc <cmb247@cam.ac.uk>
2024-07-29 16:42:38 +0100

    mark position of points

--- Image_Processing_Scripts/ellipse_plot_main.py
+++ Image_Processing_Scripts/ellipse_plot_main.py
@@ -5,13 +5,13 @@ import numpy as np
 ## MAIN SCRIPT TO PLOT ELLIPSE FORM
 data = pd.read_csv('Image_Processing_Scripts/data_entries.csv')
 print(data.columns)
-print(data.iloc[0, 2])
+##print(data.iloc[0, 2])
 
 h_def=data['deformed_contour_x']
 v_def=data['deformed_contour_y']
 h_ref=data['reflected_contour_x']
 v_ref=data['reflected_contour_y']
-print("h_def: ", h_def.iloc[0])
+
 
 hv_df = pd.DataFrame({'h_def':h_def, 'v_def':v_def, 'h_ref':h_ref, 'v_ref':v_ref})
 #print(hv_df.iloc[0,1])
@@ -19,6 +19,7 @@ total_df=pd.concat([data, hv_df], axis=1)
 
 # Transform data points such that posterior point lies on (0, 0) and anterior lies on y=0 (x axis) (for R side craniectomy) 
 #   or anterior point lies on (0,0) and posterior lies on y=0 (x axis) (for L side craniectomy)
+#       Recall baseline coords are at end of contour anterior, posterior (last two points in list in that order)
 
 
 
