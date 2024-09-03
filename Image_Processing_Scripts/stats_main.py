6740b2b18c038306099e92209dba403e24fee1c5
brassc <cmb247@cam.ac.uk>
2024-09-03 10:32:15 +0100

    first scan is dynamically selected

--- Image_Processing_Scripts/stats_main.py
+++ Image_Processing_Scripts/stats_main.py
@@ -3,6 +3,7 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.formula.api as smf
+import sys
 
 # Load the data (note this data does not contain all timepoints w NaN value if not exist - only contains timepoints w data per original data)
 data = pd.read_csv('Image_Processing_Scripts/area_data.csv')
@@ -19,6 +20,28 @@ new_df['timepoint']=pd.Categorical(new_df['timepoint'], categories=['ultra-fast'
 
 # Fit the model
 model = smf.mixedlm("area_diff ~ timepoint", new_df, groups=new_df["patient_id"], re_formula="~1")
+result = model.fit()
+# Print the summary
+print(result.summary())
+
+# Scan by 'first' scan, use this as category for comparison
+new_df=new_df.sort_values(by=['patient_id','timepoint'])
+new_df['first_scan']=None
+print(new_df.head())
+# Add a column to the dataframe that indicates the first scan for each patient. first scan is either fast or ultrafast
+new_df['first_scan']=new_df.groupby('patient_id')['timepoint'].transform('first')
+new_df['scan_type']=new_df.apply(lambda row: 'first' if row['timepoint']==row['first_scan'] 
+                             else 'fast' if row['timepoint'] == 'fast'
+                             else 'acute' if row['timepoint'] == 'acute' 
+                             else '3mo' if row['timepoint'] == '3mo' 
+                             else '6mo' if row['timepoint'] == '6mo' 
+                             else '12mo' if row['timepoint'] == '12mo' 
+                             else '24mo' if row['timepoint'] == '24mo'
+                             else 'other', axis=1)
+print(new_df.head())    
+
+
+model = smf.mixedlm("area_diff ~ C(scan_type)", new_df, groups=new_df["patient_id"], re_formula="~1")
 result = model.fit()
 
 # Print the summary
