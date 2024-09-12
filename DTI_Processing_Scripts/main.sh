14f8bae9ba6f1ffeccc625fe719107a707191fa4
brassc <cmb247@cam.ac.uk>
2024-09-12 16:22:08 +0100

    search for DWI file directory complete

---
+++ DTI_Processing_Scripts/main.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+module load fsl
+
+# Define the priority of keywords in an array
+keywords=("ultra-fast" "fast" "acute" "3mo" "6mo" "12mo" "24mo")
+
+# Define patient list
+subdirectories=("19978" "12519")
+# "13198" "13782" "13990" "14324" "16754" "19344" "19575" "19978" "19981" "20174" "20651" "20942" "21221" "22725" "22785" "23348")
+
+# get location of processed DWI images
+# define the base directory
+base_dir="/home/cmb247/rds/rds-uda-2-pXaBn8E6hyM/users/cmb247/cmb247_working/DECOMPRESSION_Legacy_CB/hemi"
+
+for sub in "${subdirectories[@]}"; do
+    echo "Processing subject $sub..."
+
+    for timepoint in "${keywords[@]}"; do
+
+        # find the DTIspace dir
+        nipype_dir=$(find "$base_dir/$sub/$timepoint/" -type d -name "nipype" 2>/dev/null | head -n 1)
+
+        # Check it exists
+        if [[ -d "$nipype_dir" ]]; then
+           #echo "nipype dir for subject $sub: $nipype_dir"
+           DTIspace_dir="$nipype_dir/DATASINK/DTIspace/"
+           echo "DTIspace_dir: $DTIspace_dir"
+           # perform operations here
+
+        fi
+    done
+done
