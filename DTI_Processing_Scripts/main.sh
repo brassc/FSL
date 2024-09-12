515ea829c6d910c6319585fff316799063b30265
brassc <cmb247@cam.ac.uk>
2024-09-12 17:04:39 +0100

    add locations of dtifit files and register them to t1_bet for relevant timepoint, save to new dir

--- DTI_Processing_Scripts/main.sh
+++ DTI_Processing_Scripts/main.sh
@@ -32,20 +32,46 @@ for sub in "${subdirectories[@]}"; do
            DTI_bvec="$DTIspace_dir/dwi_proc/DTI_corrected.bvec"
            DTI_mask="$DTIspace_dir/masks/ANTS_T1_brain_mask.nii.gz"
 
-           #use this mask to do brain extraction on DTI_corr_scan. Save to new place. 
+           dtifitWLS_FA="$DTIspace_dir/dti/dtifitWLS_FA.nii.gz"
+           dtifitWLS_MD="$DTIspace_dir/dti/dtifitWLS_MD.nii.gz"
+           dtifitWLS_L1="$DTIspace_dir/dti/dtifitWLS_L1.nii.gz"
+           dtifitWLS_L2="$DTIspace_dir/dti/dtifitWLS_L2.nii.gz"
+           dtifitWLS_L3="$DTIspace_dir/dti/dtifitWLS_L3.nii.gz"
+           dtifitWLS_MO="$DTIspace_dir/dti/dtifitWLS_MO.nii.gz"
+           dtifitWLS_S0="$DTIspace_dir/dti/dtifitWLS_S0.nii.gz"
+           dtifitWLS_V1="$DTIspace_dir/dti/dtifitWLS_V1.nii.gz"
+           dtifitWLS_V2="$DTIspace_dir/dti/dtifitWLS_V2.nii.gz"
+           dtifitWLS_V3="$DTIspace_dir/dti/dtifitWLS_V3.nii.gz"
+           powermap="$DTIspace_dir/dti/powermap.nii.gz"
+           traceMap_b1000="$DTIspace_dir/dti/traceMap_b1000.nii.gz"
+
+           
            save_dir="/home/cmb247/Desktop/Project_3/BET_Extractions/$sub/dti_reg"
            if [ ! -d $save_dir ]; then
                mkdir "${save_dir}"
            fi
-           dtibet="betdti.nii.gz"
+           dtibet="$save_dir/betdti.nii.gz"
 
+           
+           #use this mask to do brain extraction on DTI_corr_scan. Save to new place. 
            fslmaths $DTI_corr_scan -mul $DTI_mask $dtibet
+
+
+
+
+
+
+           # registration (after dtifit)
+
            t1_scan_dir="/home/cmb247/Desktop/Project_3/BET_Extractions/$sub/T1w_time1_bias_corr_registered_scans/BET_Output/"
            #search scan directory for scan name containing timepoint
            t1_scan=$(find $t1_scan_dir -type f -name "*$timepoint*.nii.gz")
 
-           flirt -in $dtibet -ref $t1_scan -out"dtibet_reg.nii.gz" -omat "dtibet_reg.mat"
-
+           flirt -in $dtibet -ref $t1_scan -out "$save_dir/dtibet_reg.nii.gz" -omat "$save_dir/dtibet_reg.mat"
+           flirt -in $dtifitWLS_FA -ref $t1_scan -out "$save_dir/dtifitWLS_FA_reg.nii.gz"
+           flirt -in $dtifitWLS_MD -ref $t1_scan -out "$save_dir/dtifitWLS_MD_reg.nii.gz"
+           # further reg not required for now
+           #flirt -in $dtifitWLS_L1 -ref $t1_scan -out "$save_dir/dtifitWLS_L1_reg.nii.gz"
         fi
     done
 done
