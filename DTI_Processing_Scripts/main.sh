dfa4e44c4164ceeadd7581c1fdb7167c18bc697f
brassc <cmb247@cam.ac.uk>
2024-09-13 11:39:41 +0100

    add error handling for when nipype directory not found for a given timepoint

--- DTI_Processing_Scripts/main.sh
+++ DTI_Processing_Scripts/main.sh
@@ -3,10 +3,11 @@
 module load fsl
 
 # Define the priority of keywords in an array
-keywords=("ultra-fast" "fast" "acute" "3mo" "6mo" "12mo" "24mo")
+keywords=("ultra-fast") # "fast" "acute" "3mo" "6mo" "12mo" "24mo")
 
 # Define patient list
-subdirectories=("19978" "12519" "13198" "13782" "13990" "14324" "16754" "19344" "19575" "19978" "19981" "20174" "20651" "20942" "21221" "22725" "22785" "23348")
+subdirectories=("12519")
+# "13198" "13782" "13990" "14324" "16754" "19344" "19575" "19978" "19981" "20174" "20651" "20942" "21221" "22725" "22785" "23348")
 
 # get location of processed DWI images
 # define the base directory
@@ -19,6 +20,12 @@ for sub in "${subdirectories[@]}"; do
 
         # find the DTIspace dir
         nipype_dir=$(find "$base_dir/$sub/$timepoint/" -type d -name "nipype" 2>/dev/null | head -n 1)
+
+        # Check if the nipype directory was found
+        if [ -z "$nipype_dir" ]; then
+            echo "Error: 'nipype' directory not found for subject $sub at timepoint $timepoint"
+            continue  # To skip this timepoint and continue with the next
+        fi
 
         # Check it exists
         if [[ -d "$nipype_dir" ]]; then
@@ -52,13 +59,13 @@ for sub in "${subdirectories[@]}"; do
            if [ ! -d $save_dir ]; then
                mkdir "${save_dir}"
            fi
-           dtibet="${save_dir}betdti.nii.gz"
-           dtibet3d="${save_dir}dtibet3d.nii.gz"
+           dtibet="${save_dir}betdti_$timepoint.nii.gz"
+           dtibet3d="${save_dir}dtibet3d_$timepoint.nii.gz"
            
 
            echo "doing bet extraction on raw scan..."
            #use this mask to do brain extraction on DTI_corr_scan. Save to new place. 
-           #fslmaths $DTI_corr_scan -mul $DTI_mask $dtibet
+           fslmaths $DTI_corr_scan -mul $DTI_mask $dtibet
            echo "bet complete."
 
 
@@ -70,7 +77,13 @@ for sub in "${subdirectories[@]}"; do
            t1_scan_dir="/home/cmb247/Desktop/Project_3/BET_Extractions/$sub/T1w_time1_bias_corr_registered_scans/BET_Output/"
            #search scan directory for scan name containing timepoint
            echo "finding t1 scan..."
-           t1_scan=$(find $t1_scan_dir -type f -name "*$timepoint*.nii.gz" ! -name "*mask*.nii.gz")
+           # search preferentially for mask that has been manually modified
+           t1_scan=$(find $t1_scan_dir -type f -name "*$timepoint*modified*.nii.gz" ! -name "*mask*.nii.gz" ! -name "*segto*")
+
+           # If no file is found containing "modified", search again with just the timepoint
+           if [ -z "$t1_scan" ]; then
+               t1_scan=$(find $t1_scan_dir -type f -name "*$timepoint*.nii.gz" ! -name "*mask*.nii.gz" ! -name "*segto*")
+           fi
            # Check if the scan was found
            if [[ -n "$t1_scan" ]]; then
                echo "Scan found: $t1_scan"
@@ -90,10 +103,13 @@ for sub in "${subdirectories[@]}"; do
            
            echo "registering 2"
            flirt -in "$dtifitWLS_FA" -ref "$t1_scan" -out "${save_dir}dtifitWLS_FA_reg_$timepoint.nii.gz"
+           
            echo "registering 3"
            flirt -in "$dtifitWLS_MD" -ref "$t1_scan" -out "${save_dir}dtifitWLS_MD_reg_$timepoint.nii.gz"
+           exit
            # further reg not required for now
            #flirt -in $dtifitWLS_L1 -ref $t1_scan -out "$save_dir/dtifitWLS_L1_reg.nii.gz"
         fi
+
     done
 done
