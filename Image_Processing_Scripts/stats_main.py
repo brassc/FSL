c2696af8a1c0289d5dd38a238dfd7f02d4b63342
brassc <cmb247@cam.ac.uk>
2024-10-13 18:38:54 +0100

    change data to be Wilcoxon pairwise comparision w Holm-Bonferrroni correction - comment out mixed effects model and plotting

--- Image_Processing_Scripts/stats_main.py
+++ Image_Processing_Scripts/stats_main.py
@@ -19,6 +19,14 @@ new_df = new_df.dropna(subset=['area_diff']) # there should be no NaN values in 
 # Ensure categorical values are categorical
 new_df['timepoint']=pd.Categorical(new_df['timepoint'], categories=['ultra-fast', 'fast', 'acute', '3mo', '6mo', '12mo', '24mo'])
 
+
+
+
+
+
+
+sys.exit()
+# below here contains code for mixed effects model - not compatible with current dataset due to sparsity and small sample size
 # Fit the model
 model = smf.mixedlm("area_diff ~ timepoint", new_df, groups=new_df["patient_id"], re_formula="~1")
 result = model.fit()
