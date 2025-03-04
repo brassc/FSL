32689a805289d6f17f07e03e7a63a497a2c13298
brassc <cmb247@cam.ac.uk>
2025-03-04 14:49:00 +0000

    update plot title

--- Image_Processing_Scripts/stats_main.py
+++ Image_Processing_Scripts/stats_main.py
@@ -636,7 +636,7 @@ if pair_data:
     # Labels
     plt.yticks(range(n_comparisons), list(reversed(summary_df['comparison'])))
     #plt.axvline(x=0, color='gray', linestyle='-', linewidth=1.5, alpha=0.7)
-    plt.title('Mean Area Differences with 95% Confidence Intervals and Corrected p-values (FDR)')
+    plt.title('Mean Differences in Herniation Area Between Timepoint Pairs \nwith 95% Confidence Intervals and Corrected p-values (FDR)')
     plt.xlabel('Mean Area Difference')
     
     
