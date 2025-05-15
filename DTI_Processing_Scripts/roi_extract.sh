4c24b6f621f6f70fd5ea0e98b4cb8e1be322a5b9
brassc <cmb247@cam.ac.uk>
2025-05-15 13:14:21 +0100

    update to get new master csv in overwrite case

--- DTI_Processing_Scripts/roi_extract.sh
+++ DTI_Processing_Scripts/roi_extract.sh
@@ -298,17 +298,29 @@ echo "Writing data to CSV..."
 echo "$data_line" >> $output_csv
 
 # Write to master CSV here
-# Write DIRECTLY to the master CSV with file locking
-echo "Writing data to master CSV..."
-(
-    # Use flock for safe concurrent access
-    flock -w 30 200
-    echo "$data_line" >> "$master_csv"
-    # Explicitly flush file system buffers
-    sync
-) 200>"${master_csv}.lock"
+# # Write DIRECTLY to the master CSV with file locking
+# echo "Writing data to master CSV..."
+# (
+#     # Use flock for safe concurrent access
+#     flock -w 30 200
+#     echo "$data_line" >> "$master_csv"
+#     # Explicitly flush file system buffers
+#     sync
+# ) 200>"${master_csv}.lock"
 
-rm -f "${master_csv}.lock"
+# rm -f "${master_csv}.lock"
+
+# Before appending
+echo "About to append to master CSV: $master_csv"
+echo "Data line content: $data_line"
+
+# When appending
+echo "$data_line" >> "$master_csv"
+
+# After appending
+echo "Verifying append operation..."
+tail -n 1 "$master_csv"
+
 
 # echo "$patient_id,$timepoint,$fa_ant_r1,$fa_ant_r2,$fa_ant_r3,$fa_ant_r4,$fa_ant_r5,$fa_post_r1,$fa_post_r2,$fa_post_r3,$fa_post_r4,$fa_post_r5,$fa_base_ant_r1,$fa_base_ant_r2,$fa_base_ant_r3,$fa_base_ant_r4,$fa_base_ant_r5,$fa_base_post_r1,$fa_base_post_r2,$fa_base_post_r3,$fa_base_post_r4,$fa_base_post_r5,$md_ant_r1,$md_ant_r2,$md_ant_r3,$md_ant_r4,$md_ant_r5,$md_post_r1,$md_post_r2,$md_post_r3,$md_post_r4,$md_post_r5,$md_base_ant_r1,$md_base_ant_r2,$md_base_ant_r3,$md_base_ant_r4,$md_base_ant_r5,$md_base_post_r1,$md_base_post_r2,$md_base_post_r3,$md_base_post_r4,$md_base_post_r5" >> $output_csv
 echo "Extraction complete for patient $patient_id at timepoint $timepoint"
