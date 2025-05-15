f8af7bdbda5dff11af38c14e45c09cf6c41c8f52
brassc <cmb247@cam.ac.uk>
2025-05-15 12:59:45 +0100

    get a not harmonised but merged df

--- DTI_Processing_Scripts/harmonisation.py
+++ DTI_Processing_Scripts/harmonisation.py
@@ -180,6 +180,7 @@ def process_metrics_file(input_filename, harmonized_output_filename):
         patient_scanner_data, 
         pre_harmonised_filename
     )
+    # return
 
     ## HARMONISATION
     # Extract FA data for harmonisation
@@ -355,6 +356,6 @@ def process_metrics_file(input_filename, harmonized_output_filename):
 process_metrics_file(input_filename='DTI_Processing_Scripts/results/all_metrics_5x4vox_NEW_filtered.csv',
                      harmonized_output_filename='DTI_Processing_Scripts/merged_data_5x4vox_NEW_filtered_harmonised.csv')
 
-process_metrics_file(input_filename='DTI_Processing_Scripts/results/all_metrics_10x4vox_NEW_filtered.csv',
-                     harmonized_output_filename='DTI_Processing_Scripts/merged_data_10x4vox_NEW_filtered_harmonised.csv')
+# process_metrics_file(input_filename='DTI_Processing_Scripts/results/all_metrics_10x4vox_NEW_filtered.csv',
+#                      harmonized_output_filename='DTI_Processing_Scripts/merged_data_10x4vox_NEW_filtered_harmonised.csv')
 
