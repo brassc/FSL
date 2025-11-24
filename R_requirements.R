a0e656ea78ee09dd2abeb1d2512f7fe69c1d9937
brassc <cmb247@cam.ac.uk>
2025-11-24 10:34:27 +0000

    update requirements

---
+++ R_requirements.R
@@ -0,0 +1,10 @@
+# R Package Requirements for FSL Project
+# Complete list of all installed R packages
+#
+# Installation instructions for new computer:
+# 1. Install Python dependencies: pip install -r requirements.txt
+# 2. Install R dependencies: Rscript R_requirements.R
+#
+# This will install all R packages with their dependencies
+
+install.packages(c("backports", "base", "boot", "broom", "BWStest", "cachem", "class", "cli", "cluster", "codetools", "colorspace", "compiler", "cowplot", "cpp11", "datasets", "Deriv", "doBy", "dplyr", "emmeans", "estimability", "fansi", "farver", "fastmap", "foreign", "generics", "ggplot2", "glue", "gmp", "graphics", "grDevices", "grid", "gtable", "isoband", "KernSmooth", "kSamples", "labeling", "lattice", "lifecycle", "lme4", "lmerTest", "magrittr", "MASS", "Matrix", "memoise", "methods", "mgcv", "microbenchmark", "minqa", "modelr", "multcompView", "munsell", "mvtnorm", "nlme", "nloptr", "nnet", "numDeriv", "parallel", "pbkrtest", "pillar", "pkgconfig", "PMCMRplus", "purrr", "R6", "rbibutils", "RColorBrewer", "Rcpp", "RcppEigen", "Rdpack", "reformulas", "rlang", "Rmpfr", "rpart", "scales", "spatial", "splines", "stats", "stats4", "stringi", "stringr", "SuppDists", "survival", "tcltk", "tibble", "tidyr", "tidyselect", "tools", "utf8", "utils", "vctrs", "viridisLite", "withr"), repos="https://cran.rstudio.com/")
